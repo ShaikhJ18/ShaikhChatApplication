22e4e08c5bdea89830fd671628ca35626ed02732
ShaikhJ18 <J.shk1980@gmail.com>
2024-05-29 17:01:26 -0400

    setup connection to mongodb and began working on models

--- api/index.js
+++ api/index.js
@@ -1,13 +1,19 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const PORT = 4000
+const dotenv = require("dotenv")
+dotenv.config()
+mongoose.connect(process.env.MONGO_URL);
+const PORT = 4040
+
 const app = express()
 
 app.get('/test',(req,res)=>{
     res.json("test ok")
 })
 
-
+app.post('/register', (req,res) =>{
+    
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server Listening on ${PORT}`)
