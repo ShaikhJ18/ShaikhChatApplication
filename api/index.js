e35d8665499b4d0f7fc1adc0ad5ecd3c080075c6
ShaikhJ18 <J.shk1980@gmail.com>
2024-05-29 17:19:36 -0400

    created model for a user and implemented authentication for registration

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const jwt = require('jsonwebtoken')
+const UserModel = require('./models/User')
 dotenv.config()
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL)
+const jwtSecret = process.env.jwtSecret
 const PORT = 4040
 
 const app = express()
@@ -11,8 +14,15 @@ app.get('/test',(req,res)=>{
     res.json("test ok")
 })
 
-app.post('/register', (req,res) =>{
-    
+app.post('/register', async (req,res) =>{
+    const {username,password} = req.body
+    const createdUser = await User.create({username,password})
+    jwt.sign({userId: createdUser._id},jwtSecret, (err,token) =>{
+        if (err) throw err
+        res.cookie('token',token).status(201).json('ok')
+    }
+
+    )
 })
 
 app.listen(PORT, ()=>{
